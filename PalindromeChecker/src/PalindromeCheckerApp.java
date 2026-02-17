25f26f619c6184ad665216ca1f8a87f400657167
dm3818-pixel <dm3818@srmist.edu.in>
2026-02-17 10:53:21 +0530

    UC1: Application Entry & Welcome Message

---
+++ PalindromeChecker/src/PalindromeCheckerApp.java
@@ -0,0 +1,19 @@
+
+public class PalindromeCheckerApp {
+    static void main(String[] args) {
+        /*
+        *
+        #USE CASE 1:
+        *
+        *This program can now display the welcome message and version id, and successful initialization
+        message.
+        *
+        @author: Jeyanth_S
+        @version: v1.0
+         */
+        System.out.println("Welcome to the Palindrome Checker App");
+        System.out.println("Version 1.0");
+        System.out.println("System initialized successfully");
+
+    }
+}
