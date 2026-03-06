4b6d630e74a42198728c7b48584a53c4bb6a022b
jeyxnth <notjeyanth@gmail.com>
2026-03-06 21:56:55 +0530

    UC13: Performance Comparison

--- PalindromeChecker/src/PalindromeCheckerApp.java
+++ PalindromeChecker/src/PalindromeCheckerApp.java
@@ -1,53 +1,33 @@
-import java.util.Scanner;
 import java.util.Stack;
 import java.util.Deque;
 import java.util.ArrayDeque;
 
-/**
- * USE CASE: 12
- * This program can Dynamically choose what strategy to use to accomplish the task
- *
- * @Author: Jeyanth_S
- * @Version: v1.12
- */
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+        String input = "level";
 
-        System.out.println("=== Palindrome Checker (Strategy Pattern) ===");
-        System.out.print("Enter a string: ");
-        String input = scanner.nextLine();
+        System.out.println("Input : " + input);
 
-        System.out.println("\nChoose Algorithm:");
-        System.out.println("1. Stack Strategy (LIFO)");
-        System.out.println("2. Deque Strategy (Two-End Comparison)");
-        System.out.print("Enter choice: ");
+        // Choose strategy (StackStrategy here)
+        PalindromeStrategy strategy = new StackStrategy();
 
-        int choice = scanner.nextInt();
-
-        PalindromeStrategy strategy;
-
-        if (choice == 1) {
-            strategy = new StackStrategy();
-        } else {
-            strategy = new DequeStrategy();
-        }
+        long startTime = System.nanoTime();
 
         boolean result = strategy.check(input);
 
-        if (result) {
-            System.out.println("\nResult: The string is a PALINDROME.");
-        } else {
-            System.out.println("\nResult: The string is NOT a palindrome.");
-        }
+        long endTime = System.nanoTime();
 
-        scanner.close();
+        long executionTime = endTime - startTime;
+
+        System.out.println("Is Palindrome? : " + result);
+        System.out.println("Execution Time : " + executionTime + " ns");
     }
 }
 
 
+/* Strategy Interface */
 interface PalindromeStrategy {
 
     boolean check(String input);
@@ -55,20 +35,18 @@ interface PalindromeStrategy {
 }
 
 
+/* Stack Based Strategy */
 class StackStrategy implements PalindromeStrategy {
 
     public boolean check(String input) {
 
         Stack<Character> stack = new Stack<>();
 
-        // Push characters
         for (char c : input.toCharArray()) {
             stack.push(c);
         }
 
-        // Compare with popped characters
         for (char c : input.toCharArray()) {
-
             if (c != stack.pop()) {
                 return false;
             }
@@ -79,6 +57,7 @@ class StackStrategy implements PalindromeStrategy {
 }
 
 
+/* Deque Based Strategy */
 class DequeStrategy implements PalindromeStrategy {
 
     public boolean check(String input) {
